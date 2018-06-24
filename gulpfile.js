a4c0e3987271e637e248e0c9afdd99df0ddecfd3
Vova Space <vova.space@icloud.com>
2018-06-24 18:20:51 +0300

    Added GitHub Pages deploy / Refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,109 @@
-const fs = require('fs');
-const rimraf = require('rimraf');
-const notify = require('gulp-notify');
-const browserSync = require('browser-sync').create();
+'use strict';
 
-const gulp = require('gulp');
-const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-const sassGlob = require('gulp-sass-glob');
-const concatCss = require('gulp-concat-css');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const csscomb = require('gulp-csscomb');
-const csso = require('gulp-csso');
-const sourcemaps = require('gulp-sourcemaps');
-const rename = require("gulp-rename");
-const spritesmith = require('gulp.spritesmith');
-const svgSprite = require("gulp-svg-sprites");
-const flatten = require('gulp-flatten');
+const fs = require('fs'),
+      rimraf = require('rimraf'),
+      notify = require('gulp-notify'),
+      browserSync = require('browser-sync').create();
+
+const gulp = require('gulp'),
+      pug = require('gulp-pug'),
+      sass = require('gulp-sass'),
+      sassGlob = require('gulp-sass-glob'),
+      concatCss = require('gulp-concat-css'),
+      concat = require('gulp-concat'),
+      autoprefixer = require('gulp-autoprefixer'),
+      csso = require('gulp-csso'),
+      sourcemaps = require('gulp-sourcemaps'),
+      rename = require("gulp-rename"),
+      spritesmith = require('gulp.spritesmith'),
+      svgSprite = require("gulp-svg-sprites"),
+      flatten = require('gulp-flatten'),
+      ghPages = require('gulp-gh-pages'),
+      replace = require('gulp-string-replace'),
+      addsrc = require('gulp-add-src');
 
 
 
 
 // PATHS
 
-const SRC_DIR = 'src/'
-const BUILD_DIR = 'build/'
+const SRC_DIR = 'src/',
+      BUILD_DIR = 'build/',
+      GH_PAGES_DIR = 'gulp-starter'; // Without '/' on the end
 
 const path = {
-    pug : {
-		src : SRC_DIR + 'template/**/*.pug',
-        pages : SRC_DIR + 'template/_pages/**/*.pug',
-        locals : './content.json',
+  pug : {
+		src : SRC_DIR + 'views/**/*.pug',
+    pages : SRC_DIR + 'views/_pages/**/*.pug',
+    locals : './content.json',
 		build : BUILD_DIR
 	},
     
-    scss : {
-        src : [SRC_DIR + 'style/**/*.scss', SRC_DIR + 'sections/**/*.scss'],
-        entry : SRC_DIR + 'style/main.scss',
-		build : BUILD_DIR + 'css/'
-    },
-    
-    cssFoundation : {
-        src : [
-                './node_modules/normalize.css/normalize.css'
-            ],
-        build : BUILD_DIR + 'css/'
-    },
-    
-    js : {
-        src: [SRC_DIR + 'js/**/*.js', SRC_DIR + 'sections/**/*.js'],
-        build: BUILD_DIR + 'js/'
-    },
+  scss : {
+    src : [SRC_DIR + 'style/**/*.scss', SRC_DIR + 'sections/**/*.scss'],
+    entry : SRC_DIR + 'style/main.scss',
+    build : BUILD_DIR + 'css/'
+  },
+  
+  cssFoundation : {
+    src : [
+      './node_modules/normalize.css/normalize.css'
+    ],
+    build : BUILD_DIR + 'css/'
+  },
+  
+  js : {
+    src: [SRC_DIR + 'js/**/*.js', SRC_DIR + 'sections/**/*.js'],
+    build: BUILD_DIR + 'js/'
+  },
 
-    jsFoundation : {
-        src : [
-                './node_modules/jquery/dist/jquery.js',
-                './node_modules/masonry-layout/dist/masonry.pkgd.js'
-            ],
-        build : BUILD_DIR + 'js/'
-    },
-    
-    img : {
-        src : [
-            SRC_DIR + 'img/**/*.*',
-            SRC_DIR + 'sections/**/img/*.*',
-            '!' + SRC_DIR + 'img/icons/*.+(svg|png|ico)',
-            ],
-        build : BUILD_DIR + 'img/'
-    },
-    
-    pngSprite : {
-        src : SRC_DIR + '/img/icons/*.png',
-        retinaSrcFilter : SRC_DIR + '/img/icons/*@2x.png',
-        buildImg : BUILD_DIR + 'img/',
-        imgLocation : '../img/sprite.png',
-        retinaImgLocation : '../img/sprite@2x.png',
-        buildFile: SRC_DIR + 'style/'
-    },
-    
-    svgSprite : {
-        src : SRC_DIR + '/img/icons/*.svg',
-        build : BUILD_DIR
-    },
-    
-    font : {
-        src : SRC_DIR + 'fonts/**/*.*',
-        build : BUILD_DIR + 'fonts/'
-    }
-}
+  jsFoundation : {
+    src : [
+      /* './node_modules/jquery/dist/jquery.js' */
+    ],
+    build : BUILD_DIR + 'js/'
+  },
+  
+  imgs : {
+    src : [
+      SRC_DIR + 'img/**/*.*',
+      SRC_DIR + 'sections/**/img/*.*',
+      '!' + SRC_DIR + 'img/icons/*.+(svg|png|ico)',
+      '!' + SRC_DIR + 'img/**/*.md'
+    ],
+    build : BUILD_DIR + 'img/'
+  },
+  
+  pngSprite : {
+    src : SRC_DIR + '/img/icons/*.png',
+    retinaSrcFilter : SRC_DIR + '/img/icons/*@2x.png',
+    buildImg : BUILD_DIR + 'img/',
+    imgLocation : '../img/sprite.png',
+    retinaImgLocation : '../img/sprite@2x.png',
+    buildFile: SRC_DIR + 'style/'
+  },
+  
+  svgSprite : {
+    src : SRC_DIR + '/img/icons/*.svg',
+    build : BUILD_DIR
+  },
+  
+  fonts : {
+    src : [
+      SRC_DIR + 'fonts/**/*.*',
+      '!' + SRC_DIR + 'fonts/*.md'
+    ],
+    build : BUILD_DIR + 'fonts/'
+  },
+
+  deploy : {
+    replaceFiles : BUILD_DIR + '**/*.+(html|css|js)',
+    otherFiles : [
+      BUILD_DIR + '**/*.*',
+      '!' + BUILD_DIR + '**/*.+(html|css|js)'
+    ]
+  }
+};
 
 
 
@@ -95,20 +111,18 @@ const path = {
 // TASKS
 
 gulp.task('pug', function() {
-//	var YOUR_LOCALS = './content.json';
-
-	return gulp.src(path.pug.pages)
-		.pipe(pug({
-			locals: JSON.parse(fs.readFileSync(path.pug.locals, 'utf-8')),
-			pretty : '\t'
-		}))
-        .on('error', notify.onError(function(error) {
-        return {
-          title: 'Pug',
-          message : error.message
-        }
-        }))
-		.pipe(gulp.dest(path.pug.build))
+  return gulp.src(path.pug.pages)
+    .pipe(pug({
+      locals: JSON.parse(fs.readFileSync(path.pug.locals, 'utf-8')),
+      pretty : '\t'
+    }))
+    .on('error', notify.onError(function(error) {
+      return {
+        title: 'Pug',
+        message : error.message
+      };
+    }))
+    .pipe(gulp.dest(path.pug.build));
 });
 
 
@@ -116,12 +130,14 @@ gulp.task('sass', function () {
   return gulp.src(path.scss.entry)
     .pipe(sourcemaps.init())
     .pipe(sassGlob())
-    .pipe(sass()).on('error', notify.onError({ title: 'Sass' }))
+    .pipe(sass({outputStyle: 'expanded'})).on('error', notify.onError({ title: 'Sass' }))
     .pipe(autoprefixer({ browsers :
-            ['last 3 version', '> 1%', 'ie 9', 'Opera 12.1']
+      ['last 3 version', '> 1%', 'ie 8', 'ie 9', 'Opera 12.1']
     }))
-    .pipe(csscomb())
     .pipe(rename('style.css'))
+    .pipe(gulp.dest(path.scss.build))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(csso())
     .pipe(sourcemaps.write('/'))
     .pipe(gulp.dest(path.scss.build))
     .pipe(browserSync.stream());
@@ -132,92 +148,112 @@ gulp.task('cssFoundation', function() {
   return gulp.src(path.cssFoundation.src)
     .pipe(concatCss('foundation.css'))
     .pipe(csso())
-    .pipe(gulp.dest(path.cssFoundation.build))
+    .pipe(gulp.dest(path.cssFoundation.build));
 })
 
 
-gulp.task('copyImage', function() {
-    return gulp.src(path.img.src, { since : gulp.lastRun('copyImage') })
+gulp.task('copyImages', function() {
+  return gulp.src(path.imgs.src, { since : gulp.lastRun('copyImages') })
     .pipe(flatten())
-    .pipe(gulp.dest(path.img.build));
+    .pipe(gulp.dest(path.imgs.build));
 });
 
 
 gulp.task('pngSprite', function () {
-    var spriteData = gulp.src(path.pngSprite.src)
+  let spriteData = gulp.src(path.pngSprite.src)
     .pipe(spritesmith({
-        imgName : 'sprite.png',
-        retinaSrcFilter: path.pngSprite.retinaSrcFilter,
-        retinaImgName: 'sprite@2x.png',
-        cssName : 'spritesmith.scss',
-        imgPath : path.pngSprite.imgLocation,
-        retinaImgPath : path.pngSprite.retinaImgLocation,
-        padding : 60,
-        cssVarMap : function(sprite) {
-            sprite.name = 'png-icon_' + sprite.name;
-        }
+      imgName : 'sprite.png',
+      retinaSrcFilter: path.pngSprite.retinaSrcFilter,
+      retinaImgName: 'sprite@2x.png',
+      cssName : 'spritesmith.scss',
+      imgPath : path.pngSprite.imgLocation,
+      retinaImgPath : path.pngSprite.retinaImgLocation,
+      padding : 60,
+      cssVarMap : function(sprite) {
+        sprite.name = 'png-icon_' + sprite.name;
+      }
     }));
-    
-    spriteData.img.pipe(gulp.dest(path.pngSprite.buildImg));
-    return spriteData.css.pipe(gulp.dest(path.pngSprite.buildFile));
+  
+  spriteData.img.pipe(gulp.dest(path.pngSprite.buildImg));
+  return spriteData.css.pipe(gulp.dest(path.pngSprite.buildFile));
 });
 
 
 gulp.task('svgSprite', function () {
-    return gulp.src(path.svgSprite.src)
-        .pipe(svgSprite({
-            mode : 'symbols',
-            preview : false,
-            selector : 'icon-%f',
-            svg : {
-                symbols : 'symbol_sprite.html'
-            }
-        }))
-        .pipe(gulp.dest(path.svgSprite.build));
+  return gulp.src(path.svgSprite.src)
+    .pipe(svgSprite({
+      mode : 'symbols',
+      preview : false,
+      selector : 'icon-%f',
+      svg : {
+          symbols : 'symbol_sprite.html'
+      }
+    }))
+    .pipe(gulp.dest(path.svgSprite.build));
 });
 
 
-gulp.task('copyFont', function() {
-    return gulp.src(path.font.src, { since : gulp.lastRun('copyFont') })
+gulp.task('copyFonts', function() {
+  return gulp.src(path.fonts.src, { since : gulp.lastRun('copyFonts') })
     .pipe(flatten())
-    .pipe(gulp.dest(path.font.build));
+    .pipe(gulp.dest(path.fonts.build));
 });
+
 
 gulp.task('js', function() {
-    return gulp.src(path.js.src)
+  return gulp.src(path.js.src)
+    .pipe(sourcemaps.init())
     .pipe(concat('app.js'))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(path.js.build));
 });
 
+
 gulp.task('jsFoundation', function() {
-    return gulp.src(path.jsFoundation.src)
-      .pipe(concat('foundation.js'))
-      .pipe(gulp.dest(path.jsFoundation.build))
-  })
+  return gulp.src([path.jsFoundation.src])
+    .pipe(concat('foundation.js'))
+    .pipe(gulp.dest(path.jsFoundation.build));
+});
+
+
+// 'gulp deploy' to deploying build into GitHub Pages
+gulp.task('deploy', function() {
+  return gulp.src(path.deploy.replaceFiles)
+    // Replays paths like 'src = "/img/pic.jpg"'
+    .pipe(replace(/['"]\/[A-Za-z0-9\._\/]+\.[A-Za-z]+['"]/g, function(replacement) {
+      // Save quote format
+      if (replacement[0] == '"') {
+        return '"/' + GH_PAGES_DIR + replacement.replace(/["]+/g, '') + '"'
+      }
+      return "'/" + GH_PAGES_DIR + replacement.replace(/[']+/g, '') + "'";
+    }))
+    .pipe(addsrc(path.deploy.otherFiles)) 
+    .pipe(ghPages());
+});
 
 
 gulp.task('clean', function(cb) {
-    return rimraf(BUILD_DIR, cb);
+  return rimraf(BUILD_DIR, cb);
 });
 
 
 gulp.task('watch', function() {
-    gulp.watch([path.pug.src, path.pug.locals], gulp.series('pug'));
-    gulp.watch(path.scss.src, gulp.series('sass'));
-    gulp.watch(path.js.src, gulp.series('js'));
-    gulp.watch(path.img.src, gulp.series('copyImage'));
-    gulp.watch(path.pngSprite.src, gulp.series('pngSprite'));
-    gulp.watch(path.svgSprite.src, gulp.series('svgSprite'));
+  gulp.watch([path.pug.src, path.pug.locals], gulp.series('pug'));
+  gulp.watch(path.scss.src, gulp.series('sass'));
+  gulp.watch(path.js.src, gulp.series('js'));
+  gulp.watch(path.imgs.src, gulp.series('copyImages'));
+  gulp.watch(path.pngSprite.src, gulp.series('pngSprite'));
+  gulp.watch(path.svgSprite.src, gulp.series('svgSprite'));
 });
 
 
 gulp.task('serve', function() {
-    browserSync.init({
-      open : false,
-      server : BUILD_DIR
-    });
+  browserSync.init({
+    open : false,
+    server : BUILD_DIR
+  });
 
-    browserSync.watch([BUILD_DIR + '/**/*.*', '!**/*.css'], browserSync.reload);
+  browserSync.watch([BUILD_DIR + '/**/*.*', '!**/*.css'], browserSync.reload);
 });
 
 
@@ -229,9 +265,9 @@ gulp.task('default', gulp.series(
   'clean',
   gulp.parallel(
     'cssFoundation',
-    'jsFoundation',
-    'copyImage',
-    'copyFont',
+    // 'jsFoundation',
+    'copyImages',
+    'copyFonts',
     'pngSprite',
     'svgSprite'
   ),
